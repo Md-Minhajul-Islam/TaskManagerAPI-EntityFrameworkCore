6f78348ed43087668e066d4a57757ce227acfbbc
Md Minhajul Islam <minhajulislammuhammad@gmail.com>
2026-04-14 12:33:23 +0600

    merge 09-performance

--- obj/Debug/net10.0/TaskManagerAPI-EntityFrameworkCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskManagerAPI-EntityFrameworkCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06884d0517b9135f3a05097a2dc662719db1ceda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62bd49055aca5ad853d5b120026b1f36b611fde4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
