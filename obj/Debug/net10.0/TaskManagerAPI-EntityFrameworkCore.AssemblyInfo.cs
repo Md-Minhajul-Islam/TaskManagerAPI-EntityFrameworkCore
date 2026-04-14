62bd49055aca5ad853d5b120026b1f36b611fde4
Md Minhajul Islam <minhajulislammuhammad@gmail.com>
2026-04-14 10:55:33 +0600

    docs: add Step 09 performance README

--- obj/Debug/net10.0/TaskManagerAPI-EntityFrameworkCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskManagerAPI-EntityFrameworkCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4178baa6b0fc9b7e5911d0302043ccd41582de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06884d0517b9135f3a05097a2dc662719db1ceda")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
