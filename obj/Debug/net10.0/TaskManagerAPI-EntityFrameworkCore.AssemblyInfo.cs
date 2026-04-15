0bb4949e418eac68373882bd60195038898bc5fd
Md Minhajul Islam <minhajulislammuhammad@gmail.com>
2026-04-15 23:15:02 +0600

    modified obj files

--- obj/Debug/net10.0/TaskManagerAPI-EntityFrameworkCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TaskManagerAPI-EntityFrameworkCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fec86f2819d9a6755558be23047dd2004471c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8938f493c2a0587954f9e48c3f91c6f67eea750")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI-EntityFrameworkCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
